2704fb376615000b4c4e288908f11e7f0c8b42e0
beatrizrecari@gmail.com <beatrizrecari@Beatrizs-MBP.local.tld>
2023-01-22 17:10:35 -0500

    Sunday homework

--- index2.js
+++ index2.js
@@ -64,3 +64,54 @@ console.log(lengthCheck);
 
 console.log(badWordIndex)
 console.log(storyWords.join(' '));
+
+// practicing methods in objects
+
+let spaceship = {
+  crew: {
+  captain: { 
+      name: 'Lily', 
+      degree: 'Computer Engineering', 
+      cheerTeam() { console.log('You got this!') } 
+      },
+  'chief officer': { 
+      name: 'Dan', 
+      degree: 'Aerospace Engineering', 
+      agree() { console.log('I agree, captain!') } 
+      },
+  medic: { 
+      name: 'Clementine', 
+      degree: 'Physics', 
+      announce() { console.log(`Jets on!`) } },
+  translator: {
+      name: 'Shauna', 
+      degree: 'Conservation Science', 
+      powerFuel() { console.log('The tank is full!') } 
+      }
+  }
+}; 
+
+// Write your code below
+
+for (let crewMember in spaceship.crew) {
+console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+};
+
+for (let crewMember in spaceship.crew) {
+console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+};
+
+// Factory Functions
+
+const robotFactory = (model, mobile) => {
+  return{
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log("Beep Boop");
+    }
+  }
+};
+const tinCan = robotFactory('P-500', true)
+
+tinCan.beep()
